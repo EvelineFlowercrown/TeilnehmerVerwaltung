c63548d5b743edfd940e6a7b08d63c3531a9ac3f
Eveline <eveflowercrown@gmail.com>
2025-11-20 14:36:44 +0100

    Fixed Session creation

--- lib/database.py
+++ lib/database.py
@@ -8,7 +8,6 @@ engine = create_engine(
     DATABASE_URL, echo=True, connect_args={"check_same_thread": False}
 )  # sqlite-threadsafety
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-SessionLocal = SessionLocal()
 
 
 class BaseClass(DeclarativeBase):
