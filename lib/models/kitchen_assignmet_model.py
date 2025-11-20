52dec818b394850c6dfba25a942cba042223efd6
DARLING <grimmchristoph84@gmail.com>
2025-11-20 08:31:01 +0100

    Add files via upload

---
+++ lib/models/kitchen_assignmet_model.py
@@ -0,0 +1,68 @@
+from sqlalchemy import Column, String, Integer, Date, Boolean, event,ForeignKey
+from sqlalchemy.orm import DeclarativeBase, relationship, Mapped, mapped_column
+from database import BaseClass
+from datetime import timedelta
+from typing import List
+#-----------------------------------------------------------------------------------------------------------------------
+#Kitchen Duty part off: - participants 1: N assignment - kitchen_duty 1: N assignment
+#----------------------------------------------------------------------------------------------------------------------
+class KitchenDuty(Base):
+    __tablename__ = "kitchen_dutys_table"
+
+    kd_id: Mapped[str] = mapped_column(
+        primary_key = True
+    )
+
+    kd_start: Mapped[Date] = mapped_column(
+        nullable = False
+    )
+
+     # Event Listener before_insert in KitchenDuty
+    @event.listens_for(KitchenDuty, "before_insert")
+    def set_kd_end(mapper, connection, target):
+        if target.kd_start and not target.kd_end:
+            target.kd_end = target.kd_start + timedelta(days=5)
+
+    kd_end: Mapped[Date] = mapped_column(
+        nullable = True
+    )
+
+    participants: Mapped[List["Participant"]] = relationship(
+        secondary = assignments_table,
+        back_populates = "kitchen_duties"
+        )
+    # -------------------------
+    # REPR
+    # -------------------------
+    def __repr__(self) -> str:
+        return (
+            f"<KitchenDuty(kd_id={self.kd_id}, "
+            f"start={self.kd_start}, end={self.kd_end})>"
+        )
+#-------------------------------------------------------------------------------------------------------------------
+# Assignment part off: - participants 1: N assignment - kitchen_duty 1: N assignment
+#--------------------------------------------------------------------------------------------------------------------
+class Assignment(Base):
+    __tablename__ = "assignments_table"
+    p_id: Mapped[int] = mapped_column(
+        ForeignKey("participants_table.p_id"),
+        primary_key = True,
+        nullable = False
+    )
+    kd_id: Mapped[int] = mapped_column(
+        ForeignKey("kitchen_dutys_table.ki_id"),
+        primary_key = True,
+        nullable = False,
+    )
+    participants: Mapped["Participant"] = relationship(
+        back_populates ="assignments"
+    )
+    kitchen_duties: Mapped["KitchenDuty"] = relationship(
+        back_populates ="assigments"
+    )
+
+    # -----------------------------------
+    # REPR
+    # -----------------------------------
+    def __repr__(self) -> str:
+        return f"<Assignment(p_id={self.p_id}, kd_id={self.kd_id})>"
---
+++ lib/models/participant_model.py
@@ -0,0 +1,107 @@
+#participant_model.py
+#from enum import Enum
+from database import BaseClass,import DateTime
+from sqlalchemy import Column, String, Integer, Date, Boolean, ForeignKey,Enum
+from sqlalchemy.orm import DeclarativeBase, relationship, Mapped, mapped_column
+
+
+
+lass Participants(BaseClass)
+    __tablename__= "participant_table"
+
+    p_id:Mapped[int] = mapped_column(
+        nullable = False,
+        primary_key = True,
+        autoincrement = True
+    )
+
+    #Name Sur and First and GDB; not Null
+    surname:Mapped[str] = mapped_column(
+        nullable= False
+    )
+    first_name:Mapped[str] = mapped_column(
+        nullable = False
+    )
+
+
+#----------------------------------------------------------------------------------------------------------------------
+#Start End Measure
+#----------------------------------------------------------------------------------------------------------------------
+    btz_start:Mapped[Date]= mapped_column(
+        nullable = False
+    )
+    btz_ende:Mapped[Date] = mapped_column(
+        nullable = False
+    )
+
+#----------------------------------------------------------------------------------------------------------------------
+#Staff Professional-Trainer and Psychosocial-Trainer
+#-----------------------------------------------------------------------------------------------------------------------
+    pt_id:Mapped[int] = mapped_column(
+        ForeignKey(
+            "pt_staff.pt_id"
+        )
+    )
+    ps_id:Mapped[int] = mapped_column(
+        ForeignKey(
+            "ps_staff.ps.id"
+        )
+    )
+
+#-----------------------------------------------------------------------------------------------------------------------
+#Needs
+#-----------------------------------------------------------------------------------------------------------------------
+    gdb: Mapped[Boolean] = mapped_column(
+        nullable = True
+    )
+    bvb:Mapped[Boolean] = mapped_column(
+        nullable = True
+    )
+    seat:Mapped[int] = mapped_column(
+        nullable = True
+    )
+    initials:Mapped[str] = mapped_column(
+        nullable = True
+    )
+    table:Mapped[int] = mapped_column(
+        nullable = True
+    )
+    #Enum for Measure
+    class Measure(Enum)
+        BT = "BT"
+        BVB = "BVB"
+        FSM = "FSM"
+        KIM = "KIM"
+
+
+    measure:Mapped[Measure] = mapped_column(
+        enum
+        nullable = False
+    )
+
+#-----------------------------------------------------------------------------------------------------------------------
+#Birthdays
+#-----------------------------------------------------------------------------------------------------------------------
+    birthday:Mapped[Date] = mapped_column(
+        datetime,
+        nullable = True
+    )
+    class BirthdayList(Enum)
+        JA = "Ja"
+        NEIN = "Nein"
+        KARTE = "Karte"
+
+    birthday_list: Mapped[BirthdayList] = mapped_column(
+        enum,
+        nullable = True
+    )
+
+
+# -------------------------
+# REPR
+# -------------------------
+    def __repr__(self) -> str:
+        return (
+            f"<Participants(p_id={self.p_id}, "
+            f"name='{self.first_name} {self.surname}', "
+            f"measure={self.measure.value})>"
