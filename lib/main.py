3234ffa2e77b915c8f39776d7a062d66c8f6f472
Eveline Siercke <evelinesiercke@gmail.com>
2025-11-21 09:55:42 +0100

    fixed import_mock_data()
    added example method for data retrieval

--- lib/main.py
+++ lib/main.py
@@ -1,30 +1,51 @@
 # main.py
+import random
+
+from sqlalchemy import select, func
 from lib import importer
 import lib.models as models
 from lib.database import engine, BaseClass, SessionLocal
 
-
 # Tabellen erzeugen
 BaseClass.metadata.create_all(bind=engine)
-
-#import_order = {
-#    "../data/ps_staff_table.csv":models.PsStaff,
-#    "../data/pt_staff_table.csv":models.PtStaff,
-#    "../data/participant_table.csv":models.Participant,
-#    "../data/internship_table.csv":models.Internship,
-#    "../data/vacation_table.csv":models.Vacation,
-#    #"../data/assignment_table.csv":models.Assignment,
-#    #"../data/kitchen_duty_table.csv":models.KitchenDuty
-#}
-
-
-#for path in import_order.keys():
-#   importer.import_csv_to_table(path,import_order[path])
-
-
+# Session Starten
 session = SessionLocal()
 
-huansohn = session.get(models.Participant,12)
-print(huansohn)
-print(huansohn.ps_staff)
+def import_mock_data():
+    import_order = {
+        "../data/ps_staff_table.csv": models.PsStaff,
+        "../data/pt_staff_table.csv": models.PtStaff,
+        "../data/participant_table.csv": models.Participant,
+        "../data/internship_table.csv": models.Internship,
+        "../data/vacation_table.csv": models.Vacation,
+        "../data/kitchen_duty_table.csv": models.KitchenDuty
+    }
 
+    # CSVs laden
+    for path, model in import_order.items():
+        importer.import_csv_to_table(path, model)
+
+    # Alle Datensätze holen
+    participants = session.scalars(select(models.Participant)).all()
+    duties = session.scalars(select(models.KitchenDuty)).all()
+
+    # Für jeden Küchendienst 3 zufällige Teilnehmer (ohne Duplikate)
+    for kd in duties:
+        picked = random.sample(participants, 3)  # garantiert 3 unterschiedliche Teilnehmer
+        kd.participants.extend(picked)
+
+    session.commit()
+
+
+
+
+#import_mock_data()
+
+#beispielmethode zur datenabfrage
+def wer_wie_oft_küchendienst():
+    all_users = session.scalars(select(models.Participant)).all()
+    for user in all_users:
+        print(f"{user.first_name + ' ' + user.surname} has performed {len(user.kitchen_duties)} kitchen duties.")
+
+
+wer_wie_oft_küchendienst()
