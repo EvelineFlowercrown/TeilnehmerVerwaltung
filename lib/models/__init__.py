83761f3c33b8f709051cbd97c5036d0b951ca8f9
Eveline Siercke <evelinesiercke@gmail.com>
2025-11-21 08:36:22 +0100

    deleted unused data and imports

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -2,7 +2,6 @@ print("Loading lib.models package")
 from .participant_model import Participant
 from .ps_staff_model import PsStaff
 from .pt_staff_model import PtStaff
-from .assignment_model import Assignment
 from .internship_model import Internship
 from .kitchen_duty_model import KitchenDuty
 from .vacation_model import Vacation
