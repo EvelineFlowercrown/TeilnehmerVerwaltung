1bbb76d1c683fbe81973bf10066f9be6b4b19d14
Eveline <eveflowercrown@gmail.com>
2025-11-24 08:44:39 +0100

    fixed moch data import paths

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ BaseClass.metadata.create_all(bind=engine)
 # Session Starten
 session = SessionLocal()
 
+
 def import_mock_data():
     import_order = {
-        "../data/ps_staff_table.csv": models.PsStaff,
-        "../data/pt_staff_table.csv": models.PtStaff,
-        "../data/participant_table.csv": models.Participant,
-        "../data/internship_table.csv": models.Internship,
-        "../data/vacation_table.csv": models.Vacation,
-        "../data/kitchen_duty_table.csv": models.KitchenDuty
+        "data/ps_staff_table.csv": models.PsStaff,
+        "data/pt_staff_table.csv": models.PtStaff,
+        "data/participant_table.csv": models.Participant,
+        "data/internship_table.csv": models.Internship,
+        "data/vacation_table.csv": models.Vacation,
+        "data/kitchen_duty_table.csv": models.KitchenDuty,
     }
 
     # CSVs laden
@@ -31,21 +32,25 @@ def import_mock_data():
 
     # Für jeden Küchendienst 3 zufällige Teilnehmer (ohne Duplikate)
     for kd in duties:
-        picked = random.sample(participants, 3)  # garantiert 3 unterschiedliche Teilnehmer
+        picked = random.sample(
+            participants, 3
+        )  # garantiert 3 unterschiedliche Teilnehmer
         kd.participants.extend(picked)
 
     session.commit()
 
 
+# import_mock_data()
 
 
-#import_mock_data()
-
-#beispielmethode zur datenabfrage
+# beispielmethode zur datenabfrage
 def wer_wie_oft_küchendienst():
     all_users = session.scalars(select(models.Participant)).all()
     for user in all_users:
-        print(f"{user.first_name + ' ' + user.surname} has performed {len(user.kitchen_duties)} kitchen duties.")
+        print(
+            f"{user.first_name + ' ' + user.surname} has performed {len(user.kitchen_duties)} kitchen duties."
+        )
+
 
 create_app()
-
+# wer_wie_oft_küchendienst()
