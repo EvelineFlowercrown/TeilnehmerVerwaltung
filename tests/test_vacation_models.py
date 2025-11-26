fb4d04910197cd40583dcc375034b690bac984d6
grimmchristoph84-commits <grimmchristoph84@gmail.com>
2025-11-26 11:50:12 +0100

    fehler behoben

--- tests/test_vacation_models.py
+++ tests/test_vacation_models.py
@@ -1,19 +1,19 @@
 from xml.sax.handler import property_interning_dict
 
 import pytest
-import datetime
+from datetime import *
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from lib.models.vacation_model import Vacation
 from lib.models.participant_model import Participant
-import lib.database
+from lib.database import BaseClass
 
 @pytest.fixture
 def session():
     """Setup für eine In-Memory SQLite DB und Session."""
-    engine = create_engine('sqlite:///:memory:')
-    lib.database.BaseClass.metadata.create_all(engine)
+    engine = create_engine("sqlite:///:memory:")
+    BaseClass.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     yield session
@@ -23,8 +23,8 @@ def session():
 def test_create_vacation(session):
     vacation = Vacation(
         p_id = Participant.p_id,
-        vacation_start = datetime.date(2023, 1, 1),
-        vacation_end =datetime.date(2023, 1, 31)
+        vacation_start = datetime(2023, 1, 1),
+        vacation_end =datetime(2023, 1, 31)
     )
     session.add(vacation)
     session.commit()
