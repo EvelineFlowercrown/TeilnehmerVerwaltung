c0feb6880a6b828901caca3d6972ab8575602b66
_darling_ <grimmchristoph84@gmail.com>
2025-11-28 10:51:42 +0100

    überarbeitet Delete und Update,

--- tests/test_vacation_models.py
+++ tests/test_vacation_models.py
@@ -1,27 +1,41 @@
-from xml.sax.handler import property_interning_dict
-
-import pytest
-from datetime import *
+# tests/test_vacation_model.py
+#from xml.sax.handler import property_interning_dict
+import datetime
 
 from lib.models.vacation_model import Vacation
-from lib.models.participant_model import Participant
 
 
-
-
-def test_create_vacation(session):
-    vacation = Vacation(
-        p_id = Participant.p_id,
-        vacation_start = datetime(2023, 1, 1),
-        vacation_end =datetime(2023, 1, 31)
+def test_vacation(session, sample_participant):
+    # CREATE
+    v = Vacation(
+        p_id=sample_participant.p_id,
+        vacation_start=datetime.date(2023, 1, 1),
+        vacation_end=datetime.date(2023, 1, 15),
     )
-    session.add(vacation)
+    session.add(v)
     session.commit()
 
-    query = session.query(vacation).filter_by(p_id= Participant.p_id ).first()
+    q = session.query(Vacation).filter_by(p_id=sample_participant.p_id).first()
+    assert q is not None
+    assert q.vacation_start == datetime.date(2023, 1, 1)
+    assert q.vacation_end == datetime.date(2023, 1, 15)
+    assert q.participant == sample_participant
 
-    assert query is not None
-    assert query.p_id == Participant.p_id
-    assert query.vacation_start == datetime.date(2023, 1, 1)
-    assert query.vacation_en == datetime.date(2023, 1, 31)
+    # UPDATE (D)
+    q.vacation_end = datetime.date(2023, 1, 20)
+    session.commit()
 
+    q2 = session.query(Vacation).filter_by(p_id=sample_participant.p_id).first()
+    assert q2.vacation_end == datetime.date(2023, 1, 20)
+
+    # DELETE (E)
+    session.delete(q2)
+    session.commit()
+
+    assert (
+        session.query(Vacation)
+        .filter_by(p_id=sample_participant.p_id,
+                   vacation_start=datetime.date(2023, 1, 1))
+        .first()
+        is None
+    )
