d5c5c78c7723edf7ade3e899f07ebaa64b6f0632
evelineflowercrown <eveflowercrown@gmail.com>
2025-12-01 11:10:48 +0100

    separated crud tests

--- tests/internship/test_internship_model.py
+++ tests/internship/test_internship_model.py
@@ -3,58 +3,120 @@ import datetime
 from lib.models.internship_model import Internship
 
 
-def test_create_internship(session, sample_participant):
-    internship = Internship(
-        p_id=sample_participant.p_id,
-        internship_start=datetime.date(2026, 1, 30),
-        internship_end=datetime.date(2026, 2, 1),
-        btz_day=Internship.BtzDay.MONDAY,
-    )
+# region: Internship-Factory
+
+# ---------------------------------------------------------
+# Internship-Factory
+# ---------------------------------------------------------
+@pytest.fixture
+def internship_factory(sample_participant):
+    """
+    Erzeugt eine Internship-Instanz für CRUD-Tests.
+    Parameter können überschrieben werden.
+    """
+    def _factory(
+        start=datetime.date(2026, 1, 30),
+        end=datetime.date(2026, 2, 1),
+        btz_day=Internship.BtzDay.MONDAY
+    ):
+        return Internship(
+            p_id=sample_participant.p_id,
+            internship_start=start,
+            internship_end=end,
+            btz_day=btz_day,
+        )
+    return _factory
+
+# endregion: Internship-Factory
+
+# region: Crud-Tests
+
+# ---------------------------------------------------------
+# CREATE
+# ---------------------------------------------------------
+def test_internship_create(session, internship_factory):
+    internship = internship_factory()
     session.add(internship)
     session.commit()
 
-    query = (
+    stored = (
         session.query(Internship)
-        .filter_by(p_id=sample_participant.p_id)
+        .filter_by(p_id=internship.p_id, internship_start=internship.internship_start)
         .first()
     )
 
-    assert query is not None
-    assert query.internship_start == datetime.date(2026, 1, 30)
-    assert query.internship_end == datetime.date(2026, 2, 1)
-    assert query.btz_day == Internship.BtzDay.MONDAY
+    assert stored is not None
+    assert stored.internship_end == datetime.date(2026, 2, 1)
+    assert stored.btz_day == Internship.BtzDay.MONDAY
 
-    # UPDATE (D)
-    query.internship_end = datetime.date(2026, 4, 15)
-    query.btz_day = Internship.BtzDay.FRIDAY
+
+# ---------------------------------------------------------
+# READ
+# ---------------------------------------------------------
+def test_internship_read(session, internship_factory):
+    internship = internship_factory()
+    session.add(internship)
     session.commit()
 
-    query2 = (
+    stored = (
         session.query(Internship)
-        .filter_by(p_id=sample_participant.p_id)
+        .filter_by(p_id=internship.p_id, internship_start=internship.internship_start)
         .first()
     )
-    assert query2.internship_end == datetime.date(2026, 4, 15)
-    assert query2.btz_day == Internship.BtzDay.FRIDAY
 
-    # DELETE (E)
-    session.delete(query2)
+    assert stored is not None
+    assert stored.internship_end == datetime.date(2026, 2, 1)
+
+
+# ---------------------------------------------------------
+# UPDATE
+# ---------------------------------------------------------
+def test_internship_update(session, internship_factory):
+    internship = internship_factory()
+    session.add(internship)
     session.commit()
-    assert (
+
+    internship.internship_end = datetime.date(2026, 4, 15)
+    internship.btz_day = Internship.BtzDay.FRIDAY
+    session.commit()
+
+    updated = (
         session.query(Internship)
-        .filter_by(p_id=sample_participant.p_id)
+        .filter_by(p_id=internship.p_id, internship_start=internship.internship_start)
         .first()
-        is None
     )
 
+    assert updated.internship_end == datetime.date(2026, 4, 15)
+    assert updated.btz_day == Internship.BtzDay.FRIDAY
 
-def test_internship_invalid_btz_day_raises(session, sample_participant):
+
+# ---------------------------------------------------------
+# DELETE
+# ---------------------------------------------------------
+def test_internship_delete(session, internship_factory):
+    internship = internship_factory()
+    session.add(internship)
+    session.commit()
+
+    session.delete(internship)
+    session.commit()
+
+    deleted = (
+        session.query(Internship)
+        .filter_by(p_id=internship.p_id, internship_start=internship.internship_start)
+        .first()
+    )
+
+    assert deleted is None
+
+# endregion: Crud-Tests
+
+
+# ---------------------------------------------------------
+# INVALID ENUM
+# ---------------------------------------------------------
+def test_internship_invalid_btz_day_raises(session, internship_factory):
     with pytest.raises(ValueError):
-        i = Internship(
-            p_id=sample_participant.p_id,
-            internship_start=datetime.date(2026, 5, 1),
-            internship_end=datetime.date(2026, 5, 31),
-            btz_day="SUNDAY",  # ungültiger Enum-Wert
-        )
-        session.add(i)
+        internship = internship_factory(btz_day="SUNDAY")  # ungültig
+        session.add(internship)
         session.commit()
