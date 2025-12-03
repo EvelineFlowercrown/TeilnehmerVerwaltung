92f85db71dc49008ea5e9b2ac696203a39058766
grimmchristoph84-commits <grimmchristoph84@gmail.com>
2025-12-03 08:21:39 +0100

    Wieder Angeschaltet :)

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,7 +1,7 @@
 print("Loading lib.models package")
-#from .participant_model import Participant
-#from .ps_staff_model import PsStaff
-#from .pt_staff_model import PtStaff
-#from .internship_model import Internship
-#from .kitchen_duty_model import KitchenDuty
-#from .vacation_model import Vacation
+from .participant_model import Participant
+from .ps_staff_model import PsStaff
+from .pt_staff_model import PtStaff
+from .internship_model import Internship
+from .kitchen_duty_model import KitchenDuty
+from .vacation_model import Vacation
